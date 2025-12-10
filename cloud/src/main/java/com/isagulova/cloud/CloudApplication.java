caa03c5b61ec331b5b72252ed62f7a7311587109
isagulovaburulsun05 <isagulovaburulsun05@gmail.com>
2025-12-10 13:32:24 +0600

    init commit

---
+++ cloud/src/main/java/com/isagulova/cloud/CloudApplication.java
@@ -0,0 +1,15 @@
+package com.isagulova.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class CloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudApplication.class, args);
+	}
+
+} // http://localhost:8888/test-client
---
+++ src/main/java/com/isagulova/clouds/CloudsApplication.java
@@ -0,0 +1,13 @@
+package com.isagulova.clouds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudsApplication.class, args);
+	}
+
+}
