9361eb0726f10b83a0a4ad35bd54597f79dfa8c2
isagulovaburulsun05 <isagulovaburulsun05@gmail.com>
2025-12-10 17:54:05 +0600

    created routing service(netflix zuul)

---
+++ src/main/java/CloudRouterApplication.java
@@ -0,0 +1,13 @@
+package com.isagulova.cloudrouter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudRouterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudRouterApplication.class, args);
+    }
+
+}
